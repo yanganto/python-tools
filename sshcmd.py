6e70feea66092d9683ab8e557ea563fc7cb66e06
Poan Yang <Poan_yang@trendmicro.com>
2015-11-25 00:09:25 +0800

    add tool to send command over ssh

---
+++ sshcmd.py
@@ -0,0 +1,33 @@
+"""
+    Send command over ssh
+
+USAGE:
+    sshcmd.py [ip:port] [username] [password] [command]
+
+EXAMPLE:
+    sshcmd.py 192.168.2.100:22 yanganto mypassword ls
+"""
+import paramiko
+import sys
+
+
+def ssh_command(addr, user, password, command):
+    ip,  port_str = addr.split(':')
+    port = int(port_str) if port_str else 22
+    client = paramiko.SSHClient()
+    # client.load_host_keys('/home/yanganto/.ssh/known_hosts')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, port, username=user, password=password)
+    ssh_session = client.get_transport().open_session()
+
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        #print(ssh_session.recv(1024))
+    return ssh_session.recv(1024)
+
+if __name__ == '__main__':
+    if len(sys.argv[1:]) != 4:
+        print(__doc__)
+        sys.exit(0)
+    else:
+        ssh_command(*sys.argv[1:])
