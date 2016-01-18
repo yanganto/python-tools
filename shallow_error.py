66126ce9c54a88820be06859a6d7987e0cc16568
ext_poany <poan_yang@trendmicro.com>
2016-01-19 00:00:10 +0800

    modify the shallow error

--- shallow_error.py
+++ shallow_error.py
@@ -4,5 +4,18 @@ import error
 try:
     error.deep_error()
 except:
+    print(type(sys.exc_info()[1]))
+    print(sys.exc_info()[1])
+    print("#"*10)
+
+    print(type(sys.exc_info()[2].tb_frame.f_code.co_filename))
     print(sys.exc_info()[2].tb_frame.f_code.co_filename)
+    print("#"*10)
+
+    print(type(sys.exc_info()[2].tb_lineno))
     print(sys.exc_info()[2].tb_lineno)
+    print("#"*10)
+
+    print(type(traceback.format_stack()))
+    print(''.join(traceback.format_stack()))
+    print("#"*10)
